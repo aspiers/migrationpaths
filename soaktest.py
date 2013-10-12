e1e1ee5d04099d5d5d77169dec925169634a5327
Adam Spiers <aspiers@suse.com>
2013-10-12 20:54:43 +0100

    shrink soaktest dimensions until it's stable

--- soaktest.py
+++ soaktest.py
@@ -13,7 +13,7 @@ from aspiers import VMPoolAdamPathFinder
 STRATEGY = VMPoolAdamPathFinder
 
 while True:
-    stateA, stateB, expected_path = testcases.random.identical_hosts()
+    stateA, stateB, expected_path = testcases.random.identical_hosts(2, 4)
 
     path_finder = STRATEGY(stateA, stateB)
     try:
